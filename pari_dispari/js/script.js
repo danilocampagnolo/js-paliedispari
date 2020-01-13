46714a130b79e306b262a4a9b00e61a4b5653d30
Danilo <danilo.campagnolo.93@gmail.com>
2020-01-13 15:56:58 +0100

    somma

--- pari_dispari/js/script.js
+++ pari_dispari/js/script.js
@@ -1,9 +1,15 @@
 // scegliere pari o dispari
-
+var pariDispari = prompt("scegli pari o dispari");
+console.log(pariDispari);
 // scegliere numero da 1 a 5
-
+var userNumber = parseInt(prompt("scegli un numero da 1 a 5"));
+console.log(userNumber);
 // generare numero random da 1 a 5
-
+var randomNumber = Math.floor(Math.random() * 5) + 1;
+console.log(randomNumber);
 // somma i due numeri
+var somma = userNumber + randomNumber;
+console.log(somma);
+
 
 // vincitore
