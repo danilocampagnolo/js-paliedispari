010cf3856c98aee515c9b8c395b46221210c2997
Danilo <danilo.campagnolo.93@gmail.com>
2020-01-13 15:11:07 +0100

    start esercizio1

---
+++ esercizio1/js/script.js
@@ -0,0 +1,15 @@
+var parola = prompt("inserisci una parola");
+var arrayParola = [];
+var messaggio;
+
+// rendere la parola un array
+for (var i = 0; i < parola.length; i++) {
+  console.log(parola[i]);
+  arrayParola.push(parola[i]);
+  console.log(arrayParola);
+}
+
+// parola.reverse
+
+
+// confronto
